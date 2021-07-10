8ac1ffa38e2c86b93c581cda642e4f696cba3a10
Vaughn Pole <vjp2109@columbia.edu>
2021-07-10 11:28:06 -0400

    my version of cycle 0

--- client/redux/store.js
+++ client/redux/store.js
@@ -4,18 +4,33 @@ import axios from 'axios';
 import thunkMiddleware from 'redux-thunk';
 
 // ACTION TYPES go here:
-
+const GOT_STUDENTS = 'GOT_STUDENTS';
 
 // ACTION CREATORS go here:
-
+export const gotStudents = () => ({
+  type: GOT_STUDENTS
+})
 
 // THUNK CREATORS go here:
+export const fetchStudents = (students) => {
+  return async (dispatch) => {
+    try{
+    const {data} = await axios.get('/api/students')
+    dispatch(gotStudents(data))
+    } catch(error) {
+      console.log(error)
+    }
+  }
+}
 
-
-const initialState = {};
+const initialState = {
+  students: []
+};
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case GOT_STUDENTS:
+      return action.students
     default:
       return state;
   }
@@ -24,7 +39,7 @@ const reducer = (state = initialState, action) => {
 const store = createStore(reducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
 
 // dispatch your own actions here to test your store functionality:
-store.dispatch({type: 'HELLO_WORLD'})
+store.dispatch({type: 'GOT_STUDENTS'})
 
 // ^ you can see the logs above in your console, thanks to redux-logger!
 
